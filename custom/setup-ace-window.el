6003b9accd5326f4bdc5e58060d48520f1cdc3d1
Prabhu Sammandam <prabhusammandam@gmail.com>
2021-06-03 12:04:10 +0530

    deleted old configs

--- custom/setup-ace-window.el
+++
@@ -1,9 +0,0 @@
-(use-package ace-window
-  :ensure ace-window
-  :bind
-  ("M-o" . ace-window)
-  :config
-;;  (setq aw-dispatch-always t)
-;;  (setq aw-ignore-current t)
-  )
-(provide 'setup-ace-window)
--- custom/setup-highlight-symbol.el
+++
@@ -1,4 +0,0 @@
-(use-package highlight-symbol)
-
-
-(provide 'setup-highlight-symbol)
